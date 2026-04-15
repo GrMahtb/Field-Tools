1921cf6ef09cb5b34db17e66c1d3e18156b2195e
GrMahtb <gregorb.mair@gmail.com>
2026-04-15 10:43:48 +0200

    Create apps.js

---
+++ apps.js
@@ -0,0 +1,47 @@
+window.APPS = [
+  {
+    id: "schwingungsmesser",
+    name: "Schwingungsmesser",
+    shortName: "Vibration",
+    icon: "📳",
+    subtitle: "ÖNORM S 9020",
+    url: "https://grmahtb.github.io/Schwingungsmesser/",
+    mode: "iframe"
+  },
+  {
+    id: "rammpfahl",
+    name: "Rammpfahl",
+    shortName: "Rammpfahl",
+    icon: "🔩",
+    subtitle: "Protokoll & Rd",
+    url: "https://grmahtb.github.io/Rammpfahl/",
+    mode: "iframe"
+  },
+  {
+    id: "pumpversuch",
+    name: "Pumpversuch",
+    shortName: "Pumpversuch",
+    icon: "💧",
+    subtitle: "Förder- & Schluckbrunnen",
+    url: "https://grmahtb.github.io/Pumpversuch/",
+    mode: "iframe"
+  },
+  {
+    id: "bohrprotokoll",
+    name: "Bohrprotokoll",
+    shortName: "Bohrprot.",
+    icon: "📋",
+    subtitle: "Nagel & Verpressung",
+    url: "https://grmahtb.github.io/Bohrprotokoll/",
+    mode: "iframe"
+  },
+  {
+    id: "bohrkernaufnahme",
+    name: "Bohrkernaufnahme",
+    shortName: "Bohrkern",
+    icon: "🪨",
+    subtitle: "ISO 14688",
+    url: "https://grmahtb.github.io/Bohrkernaufnahme/",
+    mode: "iframe"
+  }
+];
