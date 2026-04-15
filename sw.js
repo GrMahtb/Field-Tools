aefdf638e28e6a7d85abe6c8c8bf6c6f3d8f54a1
GrMahtb <gregorb.mair@gmail.com>
2026-04-15 10:58:33 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "htb-field-tools-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./apps.js",
+  "./manifest.webmanifest",
+  "./assets/logo-htb.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => (key !== CACHE_NAME ? caches.delete(key) : null))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
