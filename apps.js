a330ddf8a855fb6d2d08cd8cf1eb607dfa7b73d7
GrMahtb <gregorb.mair@gmail.com>
2026-04-16 08:06:10 +0200

    Update apps.js

--- apps.js
+++ apps.js
@@ -2,35 +2,30 @@ window.APPS = [
   {
     id: "schwingungsmesser",
     name: "Schwingungsmesser",
-    shortName: "Vibration",
     url: "https://grmahtb.github.io/Schwingungsmesser/",
     mode: "iframe"
   },
   {
     id: "rammpfahl",
     name: "Rammpfahl",
-    shortName: "Rammpfahl",
     url: "https://grmahtb.github.io/Rammpfahl/",
     mode: "iframe"
   },
   {
     id: "pumpversuch",
     name: "Pumpversuch",
-    shortName: "Pump",
     url: "https://grmahtb.github.io/Pumpversuch/",
     mode: "iframe"
   },
   {
     id: "bohrprotokoll",
     name: "Bohrprotokoll",
-    shortName: "Bohrprot.",
     url: "https://grmahtb.github.io/Bohrprotokoll/",
     mode: "iframe"
   },
   {
     id: "bohrkernaufnahme",
     name: "Bohrkernaufnahme",
-    shortName: "Bohrkern",
     url: "https://grmahtb.github.io/Bohrkernaufnahme/",
     mode: "iframe"
   }
